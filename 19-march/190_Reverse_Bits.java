cb319e0b7515226a324fa5bedcef2a5e03779108
Siddharth S <siddharth291206@gmail.com>
2026-03-22 18:58:10 +0530

    Update 190_Reverse_Bits.java

--- 19-march/190_Reverse_Bits.java
+++ 19-march/190_Reverse_Bits.java
@@ -1,8 +1,10 @@
 class Solution {
     public int reverseBits(int n) {
         int rev = 0;
-        for(int i = 0; i < 32; i++) {
-            rev = (rev << 1) | ((n >> i) & 1);
+        for (int i = 0; i < 32; i++) {
+            int bit = n & 1;
+            rev = (rev << 1) | bit;
+            n = n >> 1;
         }
         return rev;
     }
