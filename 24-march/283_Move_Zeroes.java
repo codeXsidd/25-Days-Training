7d8d45b8793527525b15322b0f8f5c004a0da8b7
Siddharth S <siddharth291206@gmail.com>
2026-03-25 06:40:22 +0530

    Create 283_Move_Zeroes.java

---
+++ 24-march/283_Move_Zeroes.java
@@ -0,0 +1,14 @@
+class Solution {
+    public void moveZeroes(int[] arr) {
+        int k=0;
+        for(int i=0;i<arr.length;i++){
+         if(arr[i] !=0){
+            arr[k++]=arr[i];
+         }
+        }
+        while(k<arr.length) {
+            arr[k++]=0;
+        }
+
+    }
+}
