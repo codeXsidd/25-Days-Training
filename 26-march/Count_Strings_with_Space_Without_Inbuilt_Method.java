799ec900bdfb79e4ac38dc7454cca33b9960bc90
Siddharth S <siddharth291206@gmail.com>
2026-03-27 06:25:46 +0530

    Update Count_Strings_with_Space_Without_Inbuilt_Method.java

--- 26-march/Count_Strings_with_Space_Without_Inbuilt_Method.java
+++ 26-march/Count_Strings_with_Space_Without_Inbuilt_Method.java
@@ -1,23 +1,20 @@
 import java.util.*;
-
 public class Main{
-    public static void main(String[] args){
-        String str="------Hello----Hi---";
-        char[] s=str.toCharArray();
-        int count=0;
-        Boolean flag=false;
-        for(char c:s){
-            if(c!='-'){
-                flag=true;
-            }
-            else{
-                if(flag){
-                    count++;
-                    flag=false;
-                }
-            }
-        }
-        if(flag)count++;
-        System.out.println(count);
+  public static void main(String[] args){
+    String str = "Hi Hello Good";
+    boolean flag = true;
+    for(int i=0;i<str.length();i++){
+       char ch = str.charAt(i);
+      if(ch!=' '){
+       if(flag){
+         count++;
+         flag=false;
+       }
+      }
+      else{
+        flag = true;
+      }
     }
+    System.out.print(count);
+  }
 }
