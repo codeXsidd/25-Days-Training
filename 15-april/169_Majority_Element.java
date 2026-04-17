67a6093b1ccbd60e989cda2daf7b8c55bfdaae1f
Siddharth S <siddharth291206@gmail.com>
2026-04-17 06:27:29 +0530

    Update and rename 169_Majority_Element_HashMap.java to 169_Majority_Element.java

--- 15-april/169_Majority_Element.java
+++ 15-april/169_Majority_Element.java
@@ -15,3 +15,30 @@ class Solution {
 return 0;
     }
 }
+
+
+//Time Complexity O(n)
+//Space Complexity O(1)
+// Boyer–Moore Voting Algorithm
+class Solution {
+    public int majorityElement(int[] nums) {
+        int c=0,v=0;
+        for(int n:nums){
+            if(v==0)c=n;
+            if(n==c)v++;
+            else v--;
+}
+             return c;
+    }
+
+
+
+//return mid
+
+//Time Complexity O(nlog n)
+class Solution {
+    public int majorityElement(int[] nums) {
+        Arrays.sort(nums);
+        return nums[nums.length/2];
+    }
+}
