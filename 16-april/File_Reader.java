3f20d0fd34e7967fde2b67b4a512a568e6dc8e8c
Siddharth S <siddharth291206@gmail.com>
2026-04-17 06:46:28 +0530

    Create File_Reader.java

---
+++ 16-april/File_Reader.java
@@ -0,0 +1,12 @@
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Main{
+  public static void main(String[] args){
+    FileReader f=new FileReader("test1.txt");
+    int ch;
+    while((ch=f.read())!=-1){
+      System.out.println(ch(char));
+    }
+  }
+}
